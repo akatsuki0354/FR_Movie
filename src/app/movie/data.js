5c9d561867bb1dcb351eda5aab14511856ddf738
akatsuki0354 <francogregorio2004@gmail.com>
2025-01-18 13:33:16 +0800

    FG:Movie

--- src/app/movie/data.js
+++ src/app/movie/data.js
@@ -1,30 +1,61 @@
-import { title } from "process";
-
 const movies = [
     {
-        img: 'https://firebasestorage.googleapis.com/v0/b/firetest-67b89.appspot.com/o/no%20one%20lives.jpg?alt=media&token=2087512e-6ee2-41ce-a4f3-c48d0af3541a',
         title: "No One Lives",
+        img: 'https://firebasestorage.googleapis.com/v0/b/firetest-67b89.appspot.com/o/no%20one%20lives.jpg?alt=media&token=2087512e-6ee2-41ce-a4f3-c48d0af3541a',
+        plot: "While traveling cross country, a couple (Betty and an unidentified man referred to as Driver) encounter a gang of robbers.",
         date_release: "2013",
-        plot: "While traveling cross country, a couple (Betty and an unidentified man referred to as Driver) encounter a gang of robbers. The group, led by dedicated criminal Hoag, consists of Hoag's brother Ethan, his daughter Amber, girlfriend Tamara, Amber's boyfriend Denny, and the violent Flynn. Suspecting the couple to be wealthy and wanting to redeem himself for a robbery he botched earlier, Flynn kidnaps them and has Ethan interrogate them about accessing their money. However, Betty commits suicide by cutting her throat on a knife Ethan held against her neck, which leads to the Driver breaking out of his handcuffs and killing Ethan.",
+        themes: "friendship",
+        genre: ["Horror", "Crime"]
     },
     {
-        img: 'https://firebasestorage.googleapis.com/v0/b/firetest-67b89.appspot.com/o/cure.jpg?alt=media&token=be3758f5-e931-4348-a735-bc7c73a0782a',
         title: "Cure",
+        img: 'https://firebasestorage.googleapis.com/v0/b/firetest-67b89.appspot.com/o/cure.jpg?alt=media&token=be3758f5-e931-4348-a735-bc7c73a0782a',
+        plot: "Erik is a thirteen-year-old loner who has just moved to a small town in Stillwater.Erik and his eleven-year-old neighbor Dexter, who contracted HIV through a blood transfusion, become good friends despite initial mutual dislike and their differences.",
         date_release: "1995",
-        plot: "Erik is a thirteen-year-old loner who has just moved to a small town in Stillwater, Minnesota. Accompanying Erik is his newly divorced, emotionally abusive and neglectful workaholic mother, Gail. Erik and his eleven-year-old neighbor Dexter, who contracted HIV through a blood transfusion, become good friends despite initial mutual dislike and their differences, as Erik seeks the familial relationships he grew up without in Dexter and his mother Linda. Erik hides the friendship from his mother, knowing that she won't approve due to her own prejudice regarding AIDS.",
+        genre: "Drama"
     },
     {
-        img: "https://firebasestorage.googleapis.com/v0/b/firetest-67b89.appspot.com/o/th.jpg?alt=media&token=d40104dc-f1e9-459b-a52b-14c3d2ad0c8d",
         title: "Wonder",
+        img: "https://firebasestorage.googleapis.com/v0/b/firetest-67b89.appspot.com/o/th.jpg?alt=media&token=d40104dc-f1e9-459b-a52b-14c3d2ad0c8d",
+        plot: "August Auggie Pullman is a 10-year-old boy living in Brooklyn with his parents, Isabel and Nate, older sister, Olivia Via, and their dog, Daisy. Auggie was born with a rare medical facial deformity, mandibulofacial dysostosis, and has undergone 27 different surgeries to help him function as a result.",
         date_release: "2017",
-        plot: "August Auggie Pullman is a 10-year-old boy living in Brooklyn with his parents, Isabel and Nate, older sister, Olivia Via, and their dog, Daisy. Auggie was born with a rare medical facial deformity, mandibulofacial dysostosis, and has undergone 27 different surgeries to help him function as a result, and wears an astronaut helmet when going in public. Auggie has been homeschooled by Isabel, but as he approaches fifth grade, she and Nate decide to finally enroll him at Beecher Prep, a private middle school. When the school year begins, Auggie is ostracized, but he soon forms a close friendship with his classmate, Jack Will, whom he previously met during a tour of the school.",
+        genre: "Drama"
     },
     {
-        img: "https://firebasestorage.googleapis.com/v0/b/firetest-67b89.appspot.com/o/silent%20voice.jpg?alt=media&token=731ed776-2c3c-4046-a595-5717b7687754",
         title: "Silent Voice",
+        img: "https://firebasestorage.googleapis.com/v0/b/firetest-67b89.appspot.com/o/silent%20voice.jpg?alt=media&token=731ed776-2c3c-4046-a595-5717b7687754",
+        plot: "An elementary school student named Shōya Ishida and his friends bully Shōko Nishimiya, a transfer student who was born deaf.",
         date_release: "2016",
-        plot: "An elementary school student named Shōya Ishida and his friends bully Shōko Nishimiya, a transfer student who was born deaf. When word of the bullying reaches the principal, Shōya is framed as the sole perpetrator by his friends. Shōya blames Shōko when she tries to help him, and they get into a physical altercation. The latter is subsequently transferred to another school, with Shōya keeping her notebook.",
+        genre: ["Drama", "Romance"]
+    },
+    {
+        title: "A Quiet Place 2",
+        img: "https://firebasestorage.googleapis.com/v0/b/firetest-67b89.appspot.com/o/a%20quiet%20place%202.jpg?alt=media&token=44c0bee5-4b27-43c9-b245-aab4806ec33f",
+        plot: "Sightless aliens (dubbed Death Angels) with sharp hearing and impenetrable armored skin have taken over the planet and killed most of the human population",
+        date_release: "2018",
+        genre: ["Horror", "Thriller", "Science Fiction", "Suspense"]
+    },
+    {
+        title: "A Quiet Place Day One",
+        img: "https://firebasestorage.googleapis.com/v0/b/firetest-67b89.appspot.com/o/a-quiet-place-day-one-movie-poster.jpg?alt=media&token=d009a9c0-6752-43d0-88e5-b80c2b53648e",
+        plot: "Sam, a pessimistic, terminally ill cancer patient, lives at a hospice outside New York City with her cat, Frodo. Reuben, a nurse, convinces a reluctant Sam to join a group outing to a marionette show in Manhattan. While in the city, the group notices meteor-like objects falling from the sky. ",
+        date_release: "2021",
+        genre: ["Horror", "Thriller", "Science Fiction", "Suspense"]
+    },{
+        title: "Tarot",
+        img: "https://firebasestorage.googleapis.com/v0/b/firetest-67b89.appspot.com/o/th.jpg?alt=media&token=ce1e0aba-92e8-4fe2-b477-7159df48a0e2",
+        plot: "A group of friends discovers a Tarot deck in the basement of their vacation rental. When they recklessly violate the sacred rule of Tarot readings—never use someone else's deck—they unknowingly unleash an unspeakable evil trapped within the cursed cards.",
+        date_release: "2024",
+        genre: ["Supernatural", "Horror", "Thriller"]
+    },{
+        title: "Avengers Endgame",
+        img: "https://firebasestorage.googleapis.com/v0/b/firetest-67b89.appspot.com/o/avengers%20endgame.jpg?alt=media&token=05fe1834-c98b-42ac-9c80-994fbb19ea61",
+        plot: "After the devastating events of 'Avengers: Infinity War', the Avengers assemble once more to undo the destruction caused by Thanos. They must find a way to travel through time, retrieve the Infinity Stones, and confront Thanos in a final battle to restore order to the universe.",
+        date_release: "2019",
+        genre: ["Action", "Adventure", "Science Fiction", "Superhero"]
     }
+    
+    
 ];
 
 export default movies;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,43 @@ import movies from './movie/data';  // Ensure the correct path
 import '../style/movie.css'
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 import { Container } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
 
+
+
+const HtmlTooltip = styled(({ className, ...props }: TooltipProps) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: '#f5f5f9',
+    color: 'rgba(0, 0, 0, 0.87)',
+    // maxWidth: 220,
+    fontSize: theme.typography.pxToRem(12),
+    border: '1px solid #dadde9',
+  },
+}));
 export default function Home() {
+
   return (
     <div className=''>
       <div className='nav'>
-        <nav className='flex justify-between p-3 bg-sky-500 mb-3'>
-          <div className="select text-white ">
+        <nav className='flex justify-between flex-wrap p-3 bg-sky-500 mb-3'>
+          <div className="select flex-wrap flex justify-center text-white ">
             <div className='logo'>
-              <h1 className='text-2xl font-bold'><span>FG</span> Movie</h1>
+              <h1 className='text-2xl font-bold'><PlayCircleIcon className='-mt-1'/><span>FG</span></h1>
             </div>
-            <div>
+            <div className='ml-5 font-semibold flex justify-center gap-3 mt-1'>
+              <a href="">Movies</a>
+              <a href="">TV-Series</a>
+              <a href="">Genre</a>
             </div>
           </div>
           <div>
-            <input type="search" placeholder='Search..' className='pl-3 p-1 w-64 focus:outline-none rounded-lg' />
+            <input type="search" placeholder='Search..' className='pl-3 p-1 mt-1 w-64 focus:outline-none rounded-lg' />
           </div>
         </nav>
       </div>
@@ -25,28 +47,35 @@ export default function Home() {
         <div className='grid place-items-center sm:grid-cols-2  md:grid-cols-3 gap-2  lg:grid-cols-4 '>
           {movies.map((movie, index) => (
             <div key={index} className='relative'>
+              <HtmlTooltip arrow
+                title={
+                  <React.Fragment>
+                    <Typography color="inherit">{movie.title}</Typography>
+                    <p>{movie.plot}</p>
+                  </React.Fragment>
+                }
+              >
               <a href="#">
                 <img
                   src={movie.img}
                   alt={`Poster of ${movie.title}`}
                   className='rounded-lg'
                 />
+                <div className='movie_title bg-gradient-to-t from-gray-950 p-2 pt-12 relative'>
+                  <div className="absolute w-full -mt-20 hidden h-full flex justify-center items-center">
+                  <PlayCircleIcon  style={{ fontSize: '60px' }} className="play_movie" />
 
-                <div className='movie_title bg-gradient-to-t from-gray-950 p-2 pt-12'>
-
-                  <div className="absolute w-full -mt-16 hidden h-full flex justify-center items-center">
-                    <PlayCircleIcon fontSize="default" className=" text-6xl play_movie" />
                   </div>
-
                   <h1 className='font-semibold'>{movie.title}</h1>
                   <p className='text-sm'>( {movie.date_release} )</p>
                   {/* <p>Plot: {movie.plot}</p> */}
                 </div>
               </a>
+            </HtmlTooltip>
             </div>
           ))}
-        </div>
-      </Container>
     </div>
+      </Container >
+    </div >
   );
 }
